eb73fc58ca39ece9dede0ff56422d2a4a29bf091
Oleg <oleg.kuzmych@outlook.com>
2023-10-17 21:40:21 -0300

    Adds support for OneOf tasks

---
+++ Extensions/OneOfAsync.cs
@@ -0,0 +1,15 @@
+﻿using OneOf;
+
+namespace Sero.Functional;
+
+public static class OneOfAsync
+{
+	public static async Task<TResult> MatchAsync<T0, T1, TResult>(
+		this Task<OneOf<T0, T1>> oneOfTask,
+		Func<T0, TResult> f0,
+		Func<T1, TResult> f1)
+	{
+		OneOf<T0, T1> oneOf = await oneOfTask;
+		return oneOf.Match(f0, f1);
+	}
+}
